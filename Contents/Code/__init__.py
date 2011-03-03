3cdfd39b42bd00c2e8ca3f7f37bbf2d85878f0d5
sander1 <sanderspies+plexapp@gmail.com>
2011-03-03 02:14:04 +0100

    Changes before initial release

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from email.utils import parsedate
 
 PLUGIN_TITLE               = 'PokerStars.tv'
-PLUGIN_PREFIX              = '/video/pokerstars-tv'
+PLUGIN_PREFIX              = '/video/pokerstarstv'
 
 # URLS
 BASE_URL                   = 'http://www.pokerstars.tv'
@@ -12,7 +12,7 @@ CHANNELS_URL               = '%s/poker-channels' % BASE_URL
 SCHEDULE_URL               = '%s/docs-pokerstarstv-schedule.html' % BASE_URL
 
 # Default artwork and icon(s)
-PLUGIN_ARTWORK             = 'art-default.png'
+PLUGIN_ARTWORK             = 'art-default.jpg'
 PLUGIN_ICON_DEFAULT        = 'icon-default.png'
 
 ###############################################################################
@@ -21,15 +21,23 @@ def Start():
   Plugin.AddViewGroup('Channels', viewMode='InfoList', mediaType='items')
   Plugin.AddViewGroup('ChannelDetails', viewMode='InfoList', mediaType='items')
   Plugin.AddViewGroup('ChannelVideos', viewMode='InfoList', mediaType='items')
+
+  MediaContainer.title1 = L('pokerstars.tv')
+  MediaContainer.art = R(PLUGIN_ARTWORK)
+
+  DirectoryItem.thumb = R(PLUGIN_ICON_DEFAULT)
+  WebVideoItem.thumb = R(PLUGIN_ICON_DEFAULT)
+
   HTTP.CacheTime = CACHE_1HOUR
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13'
 
 ###################################################################################################
 # MENUS
 ###################################################################################################
 def MainMenu():
-  dir = MediaContainer(title1=PLUGIN_TITLE,viewGroup='Channels')
+  dir = MediaContainer(viewGroup='Channels')
   dir.Append(
-    Function( DirectoryItem( Spotlight, 'Spotlight', thumb=R(PLUGIN_ICON_DEFAULT) ) )
+    Function( DirectoryItem( Spotlight, 'Spotlight' ) )
   )
   
   channels = HTML.ElementFromURL( CHANNELS_URL, errors='ignore').xpath('//*/div[@id="template"]/ul/li/div[@class="content clearfix"]/a[@class="logo"]')
@@ -57,7 +65,7 @@ def MainMenu():
   
 ###################################################################################################
 def ChannelDetails(sender,url,name,thumb_url):
-  dir = MediaContainer(title1=L('pokerstars.tv'),title2=name,viewGroup='ChannelDetails')
+  dir = MediaContainer(title2=name, viewGroup='ChannelDetails')
   # url = url.replace('-2.html', '-full-episodes.html')
   # Log( 'Getting episodes from: ' + BASE_URL + url )
   # episodes = HTML.ElementFromURL( BASE_URL + url ).xpath()
@@ -84,7 +92,7 @@ def ChannelDetails(sender,url,name,thumb_url):
   
 ###################################################################################################
 def ChannelVideos(sender,url,channel_name,name):
-  dir = MediaContainer(title1=channel_name,title2=name,viewGroup='ChannelVideos')
+  dir = MediaContainer(title2=name, viewGroup='ChannelVideos')
   videos = GetChannelVideos( url )
   
   for video in videos:
@@ -102,7 +110,7 @@ def ChannelVideos(sender,url,channel_name,name):
   
 ###################################################################################################
 def Spotlight(sender):
-  dir = MediaContainer(title1=L('pokerstars.tv'),title2=L('spotlight'),viewGroup='ChannelVideos')
+  dir = MediaContainer(title2=L('spotlight'), viewGroup='ChannelVideos')
   highlights = HTML.ElementFromURL(SCHEDULE_URL, errors='ignore').xpath('//*/div[@id="template"]/div[@id="clm-two"]/div/div[@class="content spotlight"]/ul/li')
     
   for highlight in highlights:
@@ -116,7 +124,7 @@ def Spotlight(sender):
     if is_video:
       dir.Append(
         WebVideoItem(
-          BASE_URL + url,
+          url,
           title=title,
           thumb=Function(GetThumb, thumb_url=thumb_url),
           summary=desc
@@ -142,11 +150,12 @@ def Spotlight(sender):
 # HELPERS
 ###################################################################################################
 def GetThumb(thumb_url):
-  data = HTTP.Request(thumb_url)
-  if data:
+  try:
+    data = HTTP.Request(thumb_url, cacheTime=CACHE_1MONTH).content
     return DataObject(data, 'image/png')
-  else:
+  except:
     return Redirect(R(PLUGIN_ICON_DEFAULT))
+
     
 def GetChannelVideos(url, is_sub_page=False ):
   videos = []
